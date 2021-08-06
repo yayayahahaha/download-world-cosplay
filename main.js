accde78a9c5161c08320320b258b00407c4aba2b
flyc <yayayahahahaooii@gmail.com>
2021-08-06 23:40:38 +0800

    moving

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const {
   getTotalPhotosNumber,
   getAllPhotosInfo,
   maxLimit,
-  folderDetect
+  folderDetect,
+  touchCoserPhotos
 } = require('./utils/index')
 const { cookie } = require('./utils/headers')
 
@@ -24,27 +25,3 @@ const basicQuery = { limit, member_id, page }
 
 // touchCoserPhotos
 touchCoserPhotos(19139)
-// touchCoserPhotos(135407)
-// touchCoserPhotos(28898)
-async function touchCoserPhotos(member_id, touchFile = true) {
-  folderDetect()
-  const photosNumber = await getTotalPhotosNumber(member_id)
-  console.log(`總張數: ${photosNumber}`)
-
-  const totalPages = Math.ceil(photosNumber / maxLimit)
-  console.log(`每頁 ${maxLimit} 張，總頁數: ${totalPages}`)
-
-  // 這裡的就是該coser的全部照片了
-  const photos = await getAllPhotosInfo(member_id, totalPages)
-  if (!touchFile || !photos.length) return photos
-
-  const {
-    member: { global_name, id }
-  } = photos[0]
-  const coser = global_name || id
-  const coserFolder = `result/${coser}`
-  folderDetect(coserFolder)
-  fs.writeFileSync(`./${coserFolder}/result.json`, JSON.stringify(photos, null, 2))
-
-  return photos
-}
